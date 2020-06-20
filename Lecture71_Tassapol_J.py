726362934491070fd2f1397469d336ce272bdfb6
House-chan <66866831+House-chan@users.noreply.github.com>
2020-06-20 21:43:05 +0700

    Add files via upload

---
+++ Lecture71_Tassapol_J.py
@@ -0,0 +1,32 @@
+#Use list
+menulist = []
+pricelist = []
+total = 0
+while True:
+    menuname = input("enter menu name:").capitalize()
+    if menuname == "Exit":
+        break
+    else:
+        price = int(input("Price:"))
+        menulist.append(menuname)
+        pricelist.append(price)
+        total += price
+for x in range(len(menulist)):
+    print(menulist[x],":",pricelist[x])
+print(x,":",menu[x])
+print(total)
+print(total)
+
+
+#Use dictionary
+total = 0
+menu = {}
+while True:
+menuname = input("enter menu name:").capitalize()
+if menuname == "Exit":
+break
+else:
+price = int(input("Price:"))
+menu[menuname] = price
+total += price
+for x in menu.keys():
